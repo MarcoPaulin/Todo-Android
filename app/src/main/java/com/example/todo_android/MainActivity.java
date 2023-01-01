e89a8762c31977eaabede88b12371986873d0202
MarcoPaulin <Marco.paulin@hotmail.fr>
2023-01-01 22:56:42 +0100

    ADD viewList

--- app/src/main/java/com/example/todo_android/MainActivity.java
+++ app/src/main/java/com/example/todo_android/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.todo_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.todo_android.Page.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        Intent switchActivityIntent = new Intent(this, HomeActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
